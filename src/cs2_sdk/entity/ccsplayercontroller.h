bc843e0a2330de116632d86918341074feea12b6
Hardix <hardix233@yahoo.com>
2023-10-07 22:24:07 +0200

    try1

--- src/cs2_sdk/entity/ccsplayercontroller.h
+++ src/cs2_sdk/entity/ccsplayercontroller.h
@@ -29,4 +29,5 @@ public:
 
 	SCHEMA_FIELD(CCSPlayerController_InGameMoneyServices*, m_pInGameMoneyServices)
 	SCHEMA_FIELD(CCSPlayerController_ActionTrackingServices*, m_pActionTrackingServices)
+	SCHEMA_FIELD(CCSPlayerController, m_iScore)
 };
